39aa848867a68b71a63196e101c3b94162e28a84
Mikhail Yumanov <yumanov_m@mail.ru>
2020-05-15 16:09:47 +0300

    Init milestone4

---
+++ milestone4_scopes_types/build/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/driver.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/driver.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/declarations/ClassDecl.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/declarations/ClassDecl.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/declarations/VarDecl.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/declarations/VarDecl.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/expressions/BinOpExpr.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/expressions/BinOpExpr.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/expressions/BoolConstExpr.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/expressions/BoolConstExpr.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/expressions/IdentExpr.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/expressions/IdentExpr.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/expressions/IntExpr.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/expressions/IntExpr.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/expressions/NewExpr.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/expressions/NewExpr.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/expressions/NotExpr.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/expressions/NotExpr.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/program/MainClass.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/program/MainClass.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/program/Program.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/program/Program.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/statements/AssertStmt.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/statements/AssertStmt.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/statements/AssignmentStmt.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/statements/AssignmentStmt.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/statements/ConditionalStmt.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/statements/ConditionalStmt.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/statements/LocalVarDeclStmt.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/statements/LocalVarDeclStmt.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/statements/PrintStmt.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/statements/PrintStmt.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/statements/StmtListStmt.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/statements/StmtListStmt.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/utils/BasicList.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/utils/BasicList.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/value_categories/Lvalue.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/value_categories/Lvalue.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/visitors/Interpreter.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/visitors/Interpreter.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/include/visitors/PrintVisitor.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/include/visitors/PrintVisitor.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/main.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/main.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/parser.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/parser.cpp.o"
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/scanner.cpp" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/build/CMakeFiles/Project.dir/scanner.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/parser.hh" "/compilers/compilers_course_4sem_2020/milestone2_ast_tree/parser.cpp"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ milestone4_scopes_types/build/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "../parser.cpp"
+  "../parser.hh"
+  "../scanner.cpp"
+  "CMakeFiles/Project.dir/main.cpp.o"
+  "CMakeFiles/Project.dir/driver.cpp.o"
+  "CMakeFiles/Project.dir/parser.cpp.o"
+  "CMakeFiles/Project.dir/scanner.cpp.o"
+  "CMakeFiles/Project.dir/include/program/Program.cpp.o"
+  "CMakeFiles/Project.dir/include/program/MainClass.cpp.o"
+  "CMakeFiles/Project.dir/include/statements/PrintStmt.cpp.o"
+  "CMakeFiles/Project.dir/include/statements/AssertStmt.cpp.o"
+  "CMakeFiles/Project.dir/include/statements/LocalVarDeclStmt.cpp.o"
+  "CMakeFiles/Project.dir/include/statements/StmtListStmt.cpp.o"
+  "CMakeFiles/Project.dir/include/statements/ConditionalStmt.cpp.o"
+  "CMakeFiles/Project.dir/include/statements/AssignmentStmt.cpp.o"
+  "CMakeFiles/Project.dir/include/expressions/BinOpExpr.cpp.o"
+  "CMakeFiles/Project.dir/include/expressions/BoolConstExpr.cpp.o"
+  "CMakeFiles/Project.dir/include/expressions/IntExpr.cpp.o"
+  "CMakeFiles/Project.dir/include/expressions/NewExpr.cpp.o"
+  "CMakeFiles/Project.dir/include/expressions/NotExpr.cpp.o"
+  "CMakeFiles/Project.dir/include/expressions/IdentExpr.cpp.o"
+  "CMakeFiles/Project.dir/include/utils/BasicList.cpp.o"
+  "CMakeFiles/Project.dir/include/declarations/VarDecl.cpp.o"
+  "CMakeFiles/Project.dir/include/declarations/ClassDecl.cpp.o"
+  "CMakeFiles/Project.dir/include/value_categories/Lvalue.cpp.o"
+  "CMakeFiles/Project.dir/include/visitors/PrintVisitor.cpp.o"
+  "CMakeFiles/Project.dir/include/visitors/Interpreter.cpp.o"
+  "../bin/Project.pdb"
+  "../bin/Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
